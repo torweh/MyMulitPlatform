20168c658076f032e7737a38be8065d4d315ea36
Torsten Wehrhahn <torweh@icloud.com>
2020-07-02 11:15:48 +0200

    My first iPad App

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var data = 0
     var body: some View {
-        Text("Hello, world!").padding()
+        Text("Hello, world! - \(data)").font(.largeTitle).padding()
+            .onTapGesture {
+                data+=1
+            }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView(data:55)
+            ContentView(data:48)
+                .preferredColorScheme(.dark)
+        }
     }
 }
