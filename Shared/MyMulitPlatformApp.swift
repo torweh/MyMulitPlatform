3de426fac554541b7c0d81a162dd8bbfe71a9b36
Torsten Wehrhahn <torweh@icloud.com>
2020-07-02 10:57:59 +0200

    Initial Commit

---
+++ Shared/MyMulitPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMulitPlatformApp.swift
+//  Shared
+//
+//  Created by Torsten Wehrhahn on 02.07.20.
+//
+
+import SwiftUI
+
+@main
+struct MyMulitPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
