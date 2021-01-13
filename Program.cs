e9eec61c41b6bb2907691d432b3d6000216c6ad3
Torsten Wehrhahn <torsten.wehrhahn@mpihome.com>
2021-01-13 11:12:23 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace json
+{
+    class Program
+    {
+        protected class MyType
+        {
+            public IList<string> Messages { get; set; } = new List<string>();
+        }
+
+        static void Main(string[] args)
+        {
+            MyType obj = new() { Messages = { "Hello World!", "Some more text" } };
+            JsonSerializerOptions options = new() { WriteIndented = true };
+            var jsonText = JsonSerializer.Serialize(obj, options);
+            Console.WriteLine(jsonText);
+
+            var anotherJson = "{\r\n\"Messages\": [\r\n\"Another Hello!\", \"Who wants to live forever?\", \"Bohemian Rapsody\"\r\n]\r\n}";
+            MyType anotherobj = JsonSerializer.Deserialize<MyType>(anotherJson);
+            foreach (var message in anotherobj.Messages)
+            {
+                Console.WriteLine(message);
+            }
+        
+        }
+    }
+}
